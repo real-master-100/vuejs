81e107633069fc6a70767684b5fb25bdc9b31190
strength <albertofernandez8978@gmail.com>
2024-07-24 23:35:51 +0100

    complete Vue v-for Component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,30 +1,69 @@
 <template>
   <h1>Food</h1>
+  <p>Food items are generated with v-for from the 'foods' array.</p>
   <div id="wrapper">
     <food-item
-      food-name="Apples"
-      food-desc="Apples are a type of fruit that grow on trees."/>
-    <food-item
-      food-name="Pizza"
-      food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."/>
-    <food-item
-      food-name="Rice"
-      is-favorite="true"
-      />
+      v-for="x in foods"
+      :key="x.name"
+      :food-name="x.name"
+      :food-desc="x.desc"
+      :is-favorite="x.favorite"
+    />
   </div>
+  <button @click="removeItem">Remove Item</button>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      foods: [
+        {
+          name: "Apples",
+          desc: "Apples are a type of fruit that grow on trees.",
+          favorite: true,
+        },
+        {
+          name: "Pizza",
+          desc: "Pizza has a bread base with tomato sauce, cheese, and toppings on top.",
+          favorite: false,
+        },
+        {
+          name: "Rice",
+          desc: "Rice is a type of grain that people like to eat.",
+          favorite: false,
+        },
+        {
+          name: "Fish",
+          desc: "Fish is an animal that lives in water.",
+          favorite: true,
+        },
+        {
+          name: "Cake",
+          desc: "Cake is something sweet that tastes good.",
+          favorite: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    removeItem() {
+      this.foods.splice(1, 1);
+    },
+  },
+};
+</script>
 
 <style>
-  #wrapper {
-    display: flex;
-    flex-wrap: wrap;
-  }
-  #wrapper > div {
-    border: dashed black 1px;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
+#wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+#wrapper > div {
+  border: dashed black 1px;
+  flex-basis: 120px;
+  margin: 10px;
+  padding: 10px;
+  background-color: lightgreen;
+}
 </style>
