17ce3c3c50b4994842ea0ac1b0dfed9f15012575
strength <albertofernandez8978@gmail.com>
2024-07-24 23:24:10 +0100

    complete Vue Props

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,16 +1,28 @@
 <template>
   <h1>Food</h1>
-  <food-item/>
-  <food-item/>
-  <food-item/>
+  <div id="wrapper">
+    <food-item
+      food-name="Apples"
+      food-desc="Apples are a type of fruit that grow on trees."/>
+    <food-item
+      food-name="Pizza"
+      food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."/>
+    <food-item
+      food-name="Rice"
+      is-favorite="true"
+      />
+  </div>
 </template>
 
 <script></script>
 
 <style>
-  #app > div {
+  #wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  #wrapper > div {
     border: dashed black 1px;
-    display: inline-block;
     margin: 10px;
     padding: 10px;
     background-color: lightgreen;
