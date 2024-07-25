72a1ebf8ca0727aa6a5259f8c8bce2b99c080659
strength <albertofernandez8978@gmail.com>
2024-07-25 16:47:34 +0100

    mid1 vue fallthrough attribute

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,74 +1,30 @@
 <template>
-  <h1>Food</h1>
-  <p>Food items are generated with v-for from the 'foods' array.</p>
-  <div id="wrapper">
-    <food-item
-      v-for="x in foods"
-      :key="x.name"
-      :food-name="x.name"
-      :food-desc="x.desc"
-      :is-favorite="x.favorite"
-      @toggle-favorite="receiveEmit"
+  <h3>Todo List</h3>  
+  <ul>
+    <todo-item
+      v-for="x in items"
+      :key="x"
+      :item-name="x"
+      style="background-color: lightgreen;"
     />
-  </div>
-  <button @click="removeItem">Remove Item</button>
+  </ul>
+  <input v-model="newItem">
+  <button @click="addItem">Add</button>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      foods: [
-        {
-          name: "Apples",
-          desc: "Apples are a type of fruit that grow on trees.",
-          favorite: true,
-        },
-        {
-          name: "Pizza",
-          desc: "Pizza has a bread base with tomato sauce, cheese, and toppings on top.",
-          favorite: false,
-        },
-        {
-          name: "Rice",
-          desc: "Rice is a type of grain that people like to eat.",
-          favorite: false,
-        },
-        {
-          name: "Fish",
-          desc: "Fish is an animal that lives in water.",
-          favorite: true,
-        },
-        {
-          name: "Cake",
-          desc: "Cake is something sweet that tastes good.",
-          favorite: false,
-        },
-      ],
-    };
-  },
-  methods: {
-    removeItem() {
-      this.foods.splice(1, 1);
+  export default {
+    data() {
+      return {
+        newItem: '',
+        items: ['Buy apples','Make pizza','Mow the lawn']
+      };
     },
-    receiveEmit(foodId) {
-      const foundFood = this.foods.find((food) => food.name === foodId);
-      foundFood.favorite = !foundFood.favorite;
-    },
-  },
-};
+    methods: {
+      addItem() {
+        this.items.push(this.newItem),
+        this.newItem = '';
+      }
+    }
+  }
 </script>
-
-<style>
-#wrapper {
-  display: flex;
-  flex-wrap: wrap;
-}
-#wrapper > div {
-  border: dashed black 1px;
-  flex-basis: 120px;
-  margin: 10px;
-  padding: 10px;
-  background-color: lightgreen;
-}
-</style>
