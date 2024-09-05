01137e2296fa43b44f258c8a603f7497ff67d7f2
real-master-100 <albertofernandez8978@gmail.com>
2024-09-05 16:25:41 +0100

    real-master-100_fea_scopedSlot_2024-09-05-16-25

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,67 +1,27 @@
 <template>
-  <h1>App.vue</h1>
-    <p>The component has two div tags with one slot in each.</p>
-    <slot-comp>
-      <template v-slot:topSlot>here is top</template>
-      
-      <template v-slot:bottomSlot>
-            <h4>To the bottom slot!</h4>
-            <p>
-                This p tag and the h4 tag above are directed to the bottom slot
-                with the v-slot directive used on the template tag.
-            </p>
-        </template>
-        <p>This goes into the default slot</p>
-    </slot-comp>
+  <h1>Scoped Slots</h1>
+  <p>App.vue controls how local data from the scoped slot is rendered.</p>
+  <slot-comp v-slot="food">
+    <hr>
+    <h2>{{ food.foodName }}<img :src=food.foodUrl></h2>
+    <p class="greenP">{{ food.foodDesc }}</p>
+  </slot-comp>
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            newItem: "",
-            items: ["Buy apples", "Make pizza", "Mow the lawn"],
-            foods: [
-                {
-                    name: "Apple",
-                    desc: "Apples are a type of fruit that grow on trees.",
-                    url: "img_apple.svg",
-                },
-                {
-                    name: "Pizza",
-                    desc: "Pizza has a bread base with tomato sauce, cheese, and toppings on top.",
-                    url: "img_pizza.svg",
-                },
-                {
-                    name: "Rice",
-                    desc: "Rice is a type of grain that people like to eat.",
-                    url: "img_rice.svg",
-                },
-                {
-                    name: "Fish",
-                    desc: "Fish is an animal that lives in water.",
-                    url: "img_fish.svg",
-                },
-                {
-                    name: "Cake",
-                    desc: "Cake is something sweet that tates good but is not consodered healthy.",
-                    url: "img_cake.svg",
-                },
-            ],
-        };
-    },
-    methods: {
-        addItem() {
-            this.items.push(this.newItem), (this.newItem = "");
-        },
-    },
-};
-</script>
+<script></script>
 
 <style>
-ul {
-    width: 150px;
-    list-style-type: none;
-    padding-left: 10px;
-}
-</style>
+  #app {
+    width: 300px;
+  }
+  h2, .greenP {
+    background-color: lightgreen;
+    padding: 10px;
+    margin: 0;
+  }
+  img {
+    float: right;
+    height: 70px;
+    margin-left: 10px;
+  }
+</style> 
