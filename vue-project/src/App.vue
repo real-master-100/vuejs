0e462da592377165c1556615f4f162d6373403da
real-master-100 <albertofernandez8978@gmail.com>
2024-09-05 16:16:16 +0100

    real-master-100_fea_2024-09-05-16-16

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,38 +1,67 @@
 <template>
-  <h3>Todo List</h3>  
-  <ul>
-    <todo-item
-      v-for="x in items"
-      :key="x"
-      :item-name="x"
-      style="background-color: lightgreen;"
-    />
-  </ul>
-  <input v-model="newItem">
-  <button @click="addItem">Add</button>
+  <h1>App.vue</h1>
+    <p>The component has two div tags with one slot in each.</p>
+    <slot-comp>
+      <template v-slot:topSlot>here is top</template>
+      
+      <template v-slot:bottomSlot>
+            <h4>To the bottom slot!</h4>
+            <p>
+                This p tag and the h4 tag above are directed to the bottom slot
+                with the v-slot directive used on the template tag.
+            </p>
+        </template>
+        <p>This goes into the default slot</p>
+    </slot-comp>
 </template>
 
 <script>
-  export default {
+export default {
     data() {
-      return {
-        newItem: '',
-        items: ['Buy apples','Make pizza','Mow the lawn']
-      };
+        return {
+            newItem: "",
+            items: ["Buy apples", "Make pizza", "Mow the lawn"],
+            foods: [
+                {
+                    name: "Apple",
+                    desc: "Apples are a type of fruit that grow on trees.",
+                    url: "img_apple.svg",
+                },
+                {
+                    name: "Pizza",
+                    desc: "Pizza has a bread base with tomato sauce, cheese, and toppings on top.",
+                    url: "img_pizza.svg",
+                },
+                {
+                    name: "Rice",
+                    desc: "Rice is a type of grain that people like to eat.",
+                    url: "img_rice.svg",
+                },
+                {
+                    name: "Fish",
+                    desc: "Fish is an animal that lives in water.",
+                    url: "img_fish.svg",
+                },
+                {
+                    name: "Cake",
+                    desc: "Cake is something sweet that tates good but is not consodered healthy.",
+                    url: "img_cake.svg",
+                },
+            ],
+        };
     },
     methods: {
-      addItem() {
-        this.items.push(this.newItem),
-        this.newItem = '';
-      }
-    }
-  }
+        addItem() {
+            this.items.push(this.newItem), (this.newItem = "");
+        },
+    },
+};
 </script>
 
 <style>
-  ul {
+ul {
     width: 150px;
     list-style-type: none;
     padding-left: 10px;
-  }
-</style> 
+}
+</style>
---
+++ vue-project/src/components/SlotCamp.vue
@@ -0,0 +1,13 @@
+<template>
+    <slot v-bind:lclData="data"></slot>
+  </template>
+  
+  <script>
+    export default {
+      data() {
+        return {
+          data: 'This is local data'
+        }
+      }
+    }
+  </script>
